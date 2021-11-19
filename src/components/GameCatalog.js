9c53a8411f8338bce8ab0f9f1c9a2ef551335a8a
Kalina Tsaneva <k.tsaneva1@gmail.com>
2021-11-19 16:29:13 +0200

    Made a game catalog request

--- src/components/GameCatalog.js
+++ src/components/GameCatalog.js
@@ -1,40 +1,49 @@
-
+import { useEffect, useState } from 'react';
 
 const GameCatalog = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+    .then(res => res.json())
+    .then(gamesResult => {
+      setGames(gamesResult);
+    })
+  }, []);
 
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
 
-      <div class="allGames">
-        <div class="allGames-info">
+      <div className="allGames">
+        <div className="allGames-info">
           <img src="./images/avatar-1.jpg" />
           <h6>Action</h6>
           <h2>Cover Fire</h2>
-          <a href="#" class="details-button">Details</a>
+          <a href="#" className="details-button">Details</a>
         </div>
 
       </div>
-      <div class="allGames">
-        <div class="allGames-info">
+      <div className="allGames">
+        <div className="allGames-info">
           <img src="./images/avatar-1.jpg" />
           <h6>Action</h6>
           <h2>Zombie lang</h2>
-          <a href="#" class="details-button">Details</a>
+          <a href="#" className="details-button">Details</a>
         </div>
 
       </div>
-      <div class="allGames">
-        <div class="allGames-info">
+      <div className="allGames">
+        <div className="allGames-info">
           <img src="./images/avatar-1.jpg" />
           <h6>Action</h6>
           <h2>MineCraft</h2>
-          <a href="#" class="details-button">Details</a>
+          <a href="#" className="details-button">Details</a>
         </div>
       </div>
 
 
-      <h3 class="no-articles">No articles yet</h3>
+      <h3 className="no-articles">No articles yet</h3>
     </section>
   )
 };
