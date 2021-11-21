7a03493395bdc3daebf838894123123275cefbe2
Kalina Tsaneva <k.tsaneva1@gmail.com>
2021-11-21 12:48:15 +0200

    Extracted game service

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -1,26 +1,31 @@
 import { useEffect, useState } from 'react';
-
+import * as gameService from '../../services/gameService'
 import GameCard from './GameCard';
 
 const GameCatalog = () => {
   const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-    .then(res => res.json())
-    .then(gamesResult => {
-      setGames(gamesResult);
-    })
+    setTimeout(() => {
+      gameService.getAll()
+      .then(gamesResult => {
+        setGames(gamesResult);
+      });
+    }, 1000)
   }, []);
+  
+
 
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
 
-      { games.map(game => <GameCard game={game}/>) }
+      {games.length > 0
+        ? games.map(game => <GameCard key={game._id} game={game} />)
+        : <h3 className="no-articles">No articles yet</h3>
+      }
 
-
-      <h3 className="no-articles">No articles yet</h3>
     </section>
   )
 };
